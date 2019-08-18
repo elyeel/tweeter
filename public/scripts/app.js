ec280437135f2a1c7e4ad725053a211c7f5e98f1
elyeel <elyeel@yahoo.com>
2019-08-18 00:38:28 +0000

    sanitized from script injection

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -55,37 +55,69 @@ const createDateString = function(date) {
 const createTweetElement = function(tweetData) { //this function also append it to page
   // const article = document.createElement("article"); // using vanilla JS
   // article.className = "tweet";
-  let article = $('<article>').addClass('tweet'); // using jQuery method
+  let $article = $('<article>').addClass('tweet'); // using jQuery method
+
+  // create header in tweeter container
   const newDateStr = createDateString(tweetData.created_at);
-  // let $tweet = $('<article').addClass('tweet');
-  article.append(` <header>
-  <span class="h-image">
-    <img src=${tweetData.user.avatars}>
-  </span>
-  <span class="h-name">
-    <h3>${tweetData.user.name}</h3>
-  </span>
-  <span class="h-aname">
-    <h3>${tweetData.user.handle}</h3>
-  </span>
-</header>
-<p>
-  ${tweetData.content.text}
-</p>
-<footer class="foot">
-  <span class="footer-left">
-    <h4>${newDateStr}</h4>
-  </span>
-  <span class="footer-right">
-      <i class='fas fa-flag' style='font-size:24px'></i>
-      <i class='fas fa-retweet' style='font-size:24px'></i>
-      <i class='fas fa-heart' style='font-size:24px'></i>
-  </span>
-  <!-- <h3>Footer Tweet</h3> -->
-</footer>`);
+  const $spanCHImage = $('<span>').addClass("h-image");
+  const $img = $("<img>").attr("src", tweetData.user.avatars);
+  $spanCHImage.append($img);
+  const $spanHName = $('<span>').addClass("h-name");
+  const $h3Name = $("<h3>").text(tweetData.user.name);
+  $spanHName.append($h3Name);
+  const $spanHaName = $("<span>").addClass("h-aname");
+  const $h3aName = $("<h3>").text(tweetData.user.handle);
+  $spanHaName.append($h3aName);
+  const $header = $('<header>')
+  $header.append($spanCHImage, $spanHName, $spanHaName);
+  
+  //create tweet message body in tweeter container
+  const $pTweet = $("<p>").text(tweetData.content.text);
+    
+  //creat footer in tweeter container
+  const $spanFLeft = $("<span>").addClass("footer-left");
+  $spanFLeft.append($("<h4>").text(newDateStr));
+  const $spanFRight = $("<span>").addClass("footer-right");
+  const $iFlag = $("<i>").addClass("fas fa-flag").attr("style", "font-size:24px");
+  const $iRetweet = $("<i>").addClass("fas fa-retweet").attr("style", "font-size:24px");
+  const $iHeart = $("<i>").addClass("fas fa-heart").attr("style", "font-size:24px");
+  $spanFRight.append($iFlag, $iRetweet, $iHeart);
+  const $footer = $("<footer>").addClass("foot");
+  $footer.append($spanFLeft, $spanFRight);
+
+  //append header, body, and footer to container
+  $article.append($header, $pTweet, $footer);
+  
+  //   article.append(` <header>
+              //   <span class="h-image">
+              //     <img src=${tweetData.user.avatars}>
+              //   </span>
+              //   <span class="h-name">
+              //     <h3>${tweetData.user.name}</h3>
+              //   </span>
+              //   <span class="h-aname">
+              //     <h3>${tweetData.user.handle}</h3>
+              //   </span>
+              // </header>
+              // <p>
+              //   ${tweetData.content.text}
+              // </p>
+              // <footer class="foot">
+              //   <span class="footer-left">
+              //     <h4>${newDateStr}</h4>
+              //   </span>
+              //   <span class="footer-right">
+              //       <i class='fas fa-flag' style='font-size:24px'></i>
+              //       <i class='fas fa-retweet' style='font-size:24px'></i>
+              //       <i class='fas fa-heart' style='font-size:24px'></i>
+              //   </span>
+              //   <!-- <h3>Footer Tweet</h3> -->
+              // </footer>`);
+
+
 // let main = document.querySelector("main");
 // main.className = "container";
-return article;
+return $article;
 // $(".container").append(article);
 };
 
@@ -122,7 +154,7 @@ $(document).ready(function() {
   const load = loadTweets();
   $(".compose-tweet").submit(function(event) {
     event.preventDefault();
-    const tweetLength = $('.tweet-text').val().length;
+    const tweetLength = $('.tweet-text').text($('.tweet-text').val()).length;
     const tweetText = $('.tweet-text').val();
     if (tweetText === '' || tweetText === null) {
       alert("Tweet is empty!!!");
